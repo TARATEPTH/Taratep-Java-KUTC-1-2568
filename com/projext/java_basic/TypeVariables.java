cae1a7fb13b7f24b1756979c34aca8f658fb4ccd
Taratep Daengrueangram <145983244+TARATEPTH@users.noreply.github.com>
2025-06-13 11:57:56 +0700

    ีupdate file helloword and variables

---
+++ com/projext/java_basic/TypeVariables.java
@@ -0,0 +1,13 @@
+package com.projext.java_basic;
+
+public class TypeVariables {
+    public static void main(String[] args) {
+        String name = "Taratep"; //ประกาสตัวแปรแบบข้อความ
+        int number = 7;
+        double num = 15.50;
+        char A = 'T';
+        boolean result = false;
+        System.out.println(name+number+num+A+result );
+
+    }
+}
